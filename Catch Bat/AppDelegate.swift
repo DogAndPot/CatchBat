70f33969c15a48af521e86a7daac1674554c7e6c
DogAndPot <603963560@qq.com>
2020-03-22 17:54:18 +0800

    Add animation

--- Catch Bat/AppDelegate.swift
+++ Catch Bat/AppDelegate.swift
@@ -19,11 +19,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate, NSMenuD
     @IBOutlet weak var isShowPasswd: NSButton!
     @IBOutlet weak var WiFiName: NSMenuItem!
     @IBOutlet weak var WiFiNameMenu: NSView!
+    @IBOutlet weak var WiFiStatusMenuItem: NSMenuItem!
+    @IBOutlet weak var ToggleWiFiButton: NSMenuItem!
     
     var statusBar:NSStatusItem!
     let WiFiPopup:NSAlert = NSAlert()
     var Password:String = ""
-    
+    var count:Int = 8
+    var WiFiStatus:Bool = false
+    var timer = Timer.init()
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -36,17 +40,57 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTextFieldDelegate, NSMenuD
         statusMenu.minimumWidth = CGFloat(285.0)
         statusMenu.title = ""
         statusBar = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusBar.button?.image = NSImage.init(named: "AirPortInMenu0-1")
+        statusBar.button?.image = NSImage.init(named: "AirPortOff")
         statusBar.button?.image?.isTemplate = true
         statusBar.menu = statusMenu
         statusBar.menu?.delegate = self
         
-        WiFiPop()
-
+        //WiFiPop()
+        
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @IBAction func ToggleWiFiStatus(_ sender: Any) {
+        if WiFiStatus {
+            WiFiStatus = false
+            ToggleWiFiButton.title = "开启Wi-Fi"
+            WiFiStatusMenuItem.title = "Wi-Fi: 关闭"
+            statusBar.button?.image = NSImage.init(named: "AirPortOff")
+        } else {
+            WiFiStatus = true
+            ToggleWiFiButton.title = "关闭Wi-Fi"
+            WiFiStatusMenuItem.title = "Wi-Fi: 开启"
+            timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func tick() {
+        count -= 1
+        switch count {
+        case 7:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning1")
+        case 6:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning2")
+        case 5:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning3")
+        case 4:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning4")
+        case 3:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning3")
+        case 2:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning2")
+        case 1:
+            statusBar.button?.image = NSImage.init(named: "AirPortScanning1")
+        case 0:
+            count = 8
+            timer.fireDate = Date.distantFuture
+            statusBar.button?.image = NSImage.init(named: "AirPort4")
+        default:
+            return
+        }
     }
     
     func WiFiPop() {
